f4f0925dd5911738bbf83c610202dd75d5c87e02
Flowerafro <linehenriksen0@gmail.com>
2025-10-29 10:45:06 +0100

    added dashboard to dropdown menu

--- contractflow/src/components/HeaderComponent.tsx
+++ contractflow/src/components/HeaderComponent.tsx
@@ -9,15 +9,16 @@ type HeaderProps = {
 export default function HeaderComponent({ onNavigate }: HeaderProps) {
 
     const options = [
-        {id: 1, value: '/create', label: 'Add Contract'},
-        {id: 2, value: '/tables', label: 'Tables'},
-        {id: 3, value: '/archive', label: 'Archive'},
-        {id: 4, value: '/clients', label: 'Clients'},
-        {id: 5, value: '/logout', label: 'Log out'}
+        {id: 1, value: '/home', label: 'Dashboard'},
+        {id: 2, value: '/create', label: 'Add Contract'},
+        {id: 3, value: '/tables', label: 'Tables'},
+        {id: 4, value: '/archive', label: 'Archive'},
+        {id: 5, value: '/clients', label: 'Clients'},
+        {id: 6, value: '/logout', label: 'Log out'}
     ];
 
     return (
-        <header className='bg-[var(--primary-color)] text-[var(--text-color-main)] w-full p-4 flex items-center justify-between'>
+        <header className='bg-(--primary-color) text-(--text-color-main) w-full p-4 flex items-center justify-between'>
            <div className='flex items-center gap-2'>
             <a href="/Home">
                 <Logo />
