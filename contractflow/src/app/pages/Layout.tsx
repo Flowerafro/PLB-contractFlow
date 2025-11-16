37bedb3b7c9dabef38d13533d490b046368406d1
Flowerafro <linehenriksen0@gmail.com>
2025-11-16 18:15:19 +0100

    Footer fixed
    Footer lå og fløt rundt. Endret litt på styling slik at den sksl være festet til bunn.

--- contractflow/src/app/pages/Layout.tsx
+++ contractflow/src/app/pages/Layout.tsx
@@ -6,7 +6,7 @@ import HeaderComponent from "../../components/HeaderComponent";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="relative min-h-full flex flex-col bg-[var(--bg-color)]">
+    <div className="min-h-screen flex flex-col bg-[var(--bg-color)]">
       <HeaderComponent />
 
       <main className="flex-1 p-5">
@@ -18,44 +18,4 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
   );
 }
 
-
-/* "use client";
-
-import Footer from "../../components/Footer";
-import { useState, useEffect } from "react";
-import HeaderComponent from "@/components/HeaderComponent";
-import CreateContract from "./CreateContract";
-import ClientOverview from "./ClientOverview";
-import Dashboard from "./Dashboard";
-import Tables from "./Tables";
-import Archive from "./Archive";
-
-
-export default function Layout({ children }: { children?: React.ReactNode }) {
-  
-  const [pageView, setPageView] = useState<string>("/Home");
-
-  const handlePageView = () => {
-    switch (pageView) {
-      case "/create":
-        return <CreateContract />;
-      case "/clients":
-        return <ClientOverview  />;
-      case "/tables":
-        return <Tables  />;
-      case "/archive":
-        return <Archive  />;
-        default: return children ?? <Dashboard  />;
-    }
-  } 
-
-  return (
-    <div className="relative min-h-full flex flex-col bg-[var(--bg-color)]">
-      <HeaderComponent onNavigate={(path: string) => setPageView(path)} />
-      <main className="flex-1 p-5">
-        {handlePageView()}
-      </main>
-      <Footer />
-    </div>
-  );
-} */
+/* relative h-full min-h-full flex flex-col bg-[var(--bg-color)] */
--- contractflow/src/components/Footer.tsx
+++ contractflow/src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-      <footer className="w-full bg-[var(--primary-color)] flex flex-row items-center justify-center gap-4 py-4 mt-8">
+      <footer className="w-full bg-[var(--primary-color)] flex items-center justify-center gap-4 py-4">
        <p className=" text-white text-center ">  © 2025 PLB-ContractFlow. All Rights Reserved.</p>
       </footer>
     );
