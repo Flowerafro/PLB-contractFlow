36fcd81f12361a8cd800e8236f1fff21a8301ba0
Patr1ckTS <patrick.t.skjelle@hiof.no>
2025-11-22 00:27:42 +0100

    Arbeid mot henting av filer fra R2

--- contractflow/src/app/pages/Archive.tsx
+++ contractflow/src/app/pages/Archive.tsx
@@ -1,3 +1,75 @@
+/*
+"use client";
+
+import TableGeneration from "@/features/components/TableGeneration";
+import UploadFileToR2 from "@/features/components/UploadFileToR2";
+import { hovedListenColumns } from "@/features/tables/columns/hovedListenColumns";
+import PdfModal from "@/components/PdfModal";
+import { useState } from "react";
+import type { ArchiveDocument } from "@/app/types/archiveDocument";
+import { archiveColumns } from "@/features/tables/columns/archiveColumns";
+import { fetchFromR2 } from "@/features/r2FileFetching/fetchFromR2";
+import { useEffect } from "react";
+
+export default function Archive() {
+  const [selectedRecord, setSelectedRecord] = useState<any>(null);
+
+  const [files, setFiles] = useState<ArchiveDocument[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchFromR2()
+      .then(setFiles)
+      .catch(e => setError(e.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  function handleOpenPdf(record: any) {
+    setSelectedRecord(record);
+  }
+
+  if (loading) return <div>Loading files...</div>
+  if (error) return <div>Error: {error}</div>
+
+  return (
+    <div>
+      <h1 className="font-display text-3xl md:text-5xl font-extrabold text-[var(--text-color-black)] leading-snug mb-4">Archive</h1>
+
+      {loading && (
+        <div>
+          <p>Loading data...</p>
+        </div>
+      )}
+
+      {error && (
+        <div>
+          <p>Error: {error}</p>
+        </div>
+      )}
+
+      {!loading && !error }
+
+      <TableGeneration
+        data={files}
+        columnConfig={archiveColumns}
+        onRowClick={handleOpenPdf} 
+      />
+      <UploadFileToR2 />
+      {selectedRecord && (
+        <PdfModal
+          data={selectedRecord}
+          onClose={() => setSelectedRecord(null)}
+        />
+      )}
+    </div>
+  );
+}
+function fetchR2Files() {
+  throw new Error("Function not implemented.");
+}
+*/
+
 "use client";
 
 import TableGeneration from "@/features/components/TableGeneration";
