37f20313504a7b1889c6a077eb065b4d374ae822
MapNex <54624778+MapNex@users.noreply.github.com>
2025-11-29 14:35:53 +0100

    Update seedHovedlisten.ts

--- src/db/seedHovedlisten.ts
+++ src/db/seedHovedlisten.ts
@@ -2,7 +2,7 @@ import { defineScript } from "rwsdk/worker";
 import { drizzle } from "drizzle-orm/d1";
 import { contracts, clients, principals } from "@/db/schema/schema";
 // @ts-ignore
-import hovedListenData from "@/resources/hoved_listen_paaLissom.json";
+import hovedListenData from "@/features/tables/dummyData/hoved_listen_paa_lissom.json";
 
 export const seedData = async (env?: { DB: D1Database }) => {
   try {
